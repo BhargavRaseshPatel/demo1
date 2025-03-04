de3c14d43dc514a3d014fe24fa8cab8705ea6504
Bhargav Patel <bhargav.patel@ydscanada.tech>
2025-03-04 03:32:34 -0500

    second time promise

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,59 +1,119 @@
-/********************************************************************************
-* WEB700 – Assignment 02
-*
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
-*
-* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
-*
-* Name: Bhargav Rasesh Patel 
-  Student ID: 116520248 
-  Date: January 29, 2025
-*
-********************************************************************************/
+// /********************************************************************************
+// * WEB700 – Assignment 02
+// *
+// * I declare that this assignment is my own work in accordance with Seneca's
+// * Academic Integrity Policy:
+// *
+// * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+// *
+// * Name: Bhargav Rasesh Patel 
+//   Student ID: 116520248 
+//   Date: January 29, 2025
+// *
+// ********************************************************************************/
 
-// import { Data } from '../data/setData.js'
-// import { Theme } from '../data/themeData.js'
+// // import { Data } from '../data/setData.js'
+// // import { Theme } from '../data/themeData.js'
 
-// const Data = require('../data/setData.js')
-// const Theme = require('../data/themeData.js')
+// // const Data = require('../data/setData.js')
+// // const Theme = require('../data/themeData.js')
+
+// class LegoData {
+//     sets
+
+//     constructor() {
+//         this.sets = []
+//     }
+
+//     initialize() {
+
+//         const setData = require('../data/setData.js')
+//         const themeData = require('../data/themeData.js')
+
+//         this.sets = setData.map(element => {
+//             element.theme = themeData.find((ele) => ele.id == element.theme_id).name
+//             return {...element}
+//         });
+//     }
+
+//     getAllSets() {
+//         return new Promise((resolve, reject ) => {
+//             resolve(this.sets)
+//         })
+//     }
+
+//     getSetByNum(setNum) {
+//         return new Promise((resolve, reject) => {
+//             resolve(this.sets.find((ele) => ele.set_num == setNum))
+//         }) 
+//     }
+
+//     getSetsByTheme(theme) {
+//         return new Promise((resolve, reject) => {
+//             resolve(this.sets.filter((element) => element.theme.toLowerCase().includes(theme)))
+//         }) 
+//     }
+// }
+
+// // export default LegoData;
+// module.exports = LegoData
 
 class LegoData {
-    sets
-
     constructor() {
-        this.sets = []
+        this.sets = [];
     }
 
     initialize() {
+        return new Promise((resolve, reject) => {
+            try {
+                const setData = require('../data/setData.js');
+                const themeData = require('../data/themeData.js');
 
-        const setData = require('../data/setData.js')
-        const themeData = require('../data/themeData.js')
-
-        this.sets = setData.map(element => {
-            element.theme = themeData.find((ele) => ele.id == element.theme_id).name
-            return {...element}
+                this.sets = setData.map(element => {
+                    const theme = themeData.find(ele => ele.id == element.theme_id);
+                    if (theme) {
+                        element.theme = theme.name;
+                    }
+                    return { ...element };
+                });
+                resolve();
+            } catch (error) {
+                reject("Error initializing LegoData: " + error);
+            }
         });
     }
 
     getAllSets() {
-        return new Promise((resolve, reject ) => {
-            resolve(this.sets)
-        })
+        return new Promise((resolve, reject) => {
+            if (this.sets.length > 0) {
+                resolve(this.sets);
+            } else {
+                reject("No Lego sets available");
+            }
+        });
     }
 
     getSetByNum(setNum) {
         return new Promise((resolve, reject) => {
-            resolve(this.sets.find((ele) => ele.set_num == setNum))
-        }) 
+            const set = this.sets.find(ele => ele.set_num == setNum);
+            if (set) {
+                resolve(set);
+            } else {
+                reject("Set not found");
+            }
+        });
     }
 
     getSetsByTheme(theme) {
         return new Promise((resolve, reject) => {
-            resolve(this.sets.filter((element) => element.theme.toLowerCase().includes(theme)))
-        }) 
+            const filteredSets = this.sets.filter(element => element.theme.toLowerCase().includes(theme.toLowerCase()));
+            if (filteredSets.length > 0) {
+                resolve(filteredSets);
+            } else {
+                reject("No sets found for the given theme");
+            }
+        });
     }
 }
 
-// export default LegoData;
-module.exports = LegoData
+module.exports = LegoData;
--- server.js
+++ server.js
@@ -1,75 +1,124 @@
-/********************************************************************************
-* WEB700 – Assignment 02
-*
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
-*
-* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
-*
-* Name: Bhargav Rasesh Patel 
-  Student ID: 116520248 
-  Date: February 14, 2025
+// /********************************************************************************
+// * WEB700 – Assignment 02
+// *
+// * I declare that this assignment is my own work in accordance with Seneca's
+// * Academic Integrity Policy:
+// *
+// * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+// *
+// * Name: Bhargav Rasesh Patel 
+//   Student ID: 116520248 
+//   Date: February 14, 2025
 
-  Published URL : https://demo1-blue-xi.vercel.app/
-*
-********************************************************************************/
+//   Published URL : https://demo1-blue-xi.vercel.app/
+// *
+// ********************************************************************************/
 
-const LegoData = require('./modules/legoSets')
-const legoData = new LegoData()
+// const LegoData = require('./modules/legoSets')
+// const legoData = new LegoData()
 
-const express = require('express'); // "require" the Express module
-const app = express(); // obtain the "app" object
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+// const express = require('express'); // "require" the Express module
+// const app = express(); // obtain the "app" object
+// const HTTP_PORT = process.env.PORT || 8080; // assign a port
 
-const path = require('path')
+// const path = require('path')
 
-app.use(express.static(__dirname + "public"))
+// app.use(express.static(__dirname + "public"))
 
-// start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => {
-    try {
-        legoData.initialize()
-    } catch (err) {
-        console.log("There is an error in listening the legoData initialize.")
-        console.log("Error ", err)
-    }
-    console.log(`server listening on: ${HTTP_PORT}`)
-}
-);
+// // start the server on the port and output a confirmation to the console
+// app.listen(HTTP_PORT, () => {
+//     try {
+//         legoData.initialize()
+//     } catch (err) {
+//         console.log("There is an error in listening the legoData initialize.")
+//         console.log("Error ", err)
+//     }
+//     console.log(`server listening on: ${HTTP_PORT}`)
+// }
+// );
+
+// app.get('/', (req, res) => {
+//     // res.send('Hello World!');
+//     res.sendFile(path.join(__dirname, '/views/home.html'))
+// });
+
+// app.get('/about', (req, res) => {
+//     // res.send('About the Company');
+//     res.sendFile(path.join(__dirname, '/views/about.html'))
+// });
+
+// app.get('/lego/sets', (req, res) => {
+//     const { theme } = req.query
+//     var data = null;
+//     if (theme != undefined) {
+//         data = legoData.getSetsByTheme(theme)
+//     } else {
+//         data = legoData.getAllSets().then((data) => console.log(data))
+//     }
+
+//     if (!data) {
+//         res.sendFile(path.join(__dirname, '/views/404.html'))
+//     }
+//     res.send(data)
+// })
+
+// app.get('/lego/sets/:set_num', (req, res) => {
+//     const { set_num } = req.params
+
+//     const data = legoData.getSetByNum(set_num)
+//     // console.log("Data ",data)
+//     if (!data) {
+//         res.sendFile(path.join(__dirname, '/views/404.html'))
+//         console.log(true)
+//     }
+//     res.send(data)
+// })
+
+const express = require('express');
+const path = require('path');
+const LegoData = require('./modules/legoSets');
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const legoData = new LegoData();
+legoData.initialize()
+    .then(() => {
+        app.listen(HTTP_PORT, () => {
+            console.log(`Server listening on: ${HTTP_PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error("Failed to initialize LegoData:", err);
+    });
 
 app.get('/', (req, res) => {
-    // res.send('Hello World!');
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 app.get('/about', (req, res) => {
-    // res.send('About the Company');
-    res.sendFile(path.join(__dirname, '/views/about.html'))
+    res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
 app.get('/lego/sets', (req, res) => {
-    const { theme } = req.query
-    var data = null;
-    if (theme != undefined) {
-        data = legoData.getSetsByTheme(theme)
+    const { theme } = req.query;
+    if (theme) {
+        legoData.getSetsByTheme(theme)
+            .then(data => res.json(data))
+            .catch(() => res.sendFile(path.join(__dirname, '/views/404.html')));
     } else {
-        data = legoData.getAllSets()
+        legoData.getAllSets()
+            .then(data => res.json(data))
+            .catch(() => res.sendFile(path.join(__dirname, '/views/404.html')));
     }
-
-    if (!data) {
-        res.sendFile(path.join(__dirname, '/views/404.html'))
-    }
-    res.send(data)
-})
+});
 
 app.get('/lego/sets/:set_num', (req, res) => {
-    const { set_num } = req.params
+    const { set_num } = req.params;
+    legoData.getSetByNum(set_num)
+        .then(data => res.json(data))
+        .catch(() => res.sendFile(path.join(__dirname, '/views/404.html')));
+});
 
-    const data = legoData.getSetByNum(set_num)
-    // console.log("Data ",data)
-    if (!data) {
-        res.sendFile(path.join(__dirname, '/views/404.html'))
-        console.log(true)
-    }
-    res.send(data)
-})
+module.exports = app;
